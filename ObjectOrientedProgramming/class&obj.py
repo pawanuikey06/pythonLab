a896edfe4c0c650a8ac51a0ab82bffff8bbaf7e0
Pawan Uikey <pawanuikey690@gmail.com>
2025-02-09 15:55:42 +0530

    Classes And Objects

---
+++ ObjectOrientedProgramming/class&obj.py
@@ -0,0 +1,11 @@
+from car import Car
+
+car1 =Car("BMW",2024,"Red",False)
+car2 =Car("Mustang",2025,'Blue',True)
+car3 =Car("Scarpio N",2021,"Black",True)
+
+print(car1.model,car1.year,car1.year,car1.for_sale)
+        
+
+car1.drive()
+car3.stop()
