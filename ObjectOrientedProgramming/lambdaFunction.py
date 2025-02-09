674ea31b7a5d5471ebf8632f13e37743d9ae60d7
Pawan Uikey <pawanuikey690@gmail.com>
2025-02-09 18:37:52 +0530

    lambda functions

--- ObjectOrientedProgramming/lambdaFunction.py
+++ ObjectOrientedProgramming/lambdaFunction.py
@@ -3,4 +3,16 @@
 
 double =lambda x :x**2
 
+add =lambda x ,y:x+y
+
+max_value =lambda x,y :x if x>y else y
+
+full_name =lambda x ,y:x+" "+y
+
+is_even =lambda x:x%2==0
+
 print(double(5))
+print(add(2,4))
+print(max_value(12312,4))
+print(full_name("hello","ji"))
+print(is_even(1312312))
